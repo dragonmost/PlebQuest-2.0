e04408629efc582b99f47cb4503f8b380dfca389
dragonmost <dragonmost@live.ca>
2016-07-21 19:35:09 -0400

    yolo

--- PlebQuest/Program.cs
+++ PlebQuest/Program.cs
@@ -15,7 +15,7 @@ namespace PlebQuest
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
+            //Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainMenu());
             DataBase.GetPleb("test", "test");
